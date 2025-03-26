62fb716c1f9bb5e4b7b6c477010616c6b481065c
TomTassin <ttassin150@gmail.com>
2025-03-26 19:44:53 +0100

    choix 2 et 3 fini

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -3,6 +3,7 @@ package frontiere;
 import controleur.ControlAcheterProduit;
 
 public class BoundaryAcheterProduit {
+	private BoundaryAfficherMarche boundaryAfficherMarche;
 	private ControlAcheterProduit controlAcheterProduit;
 
 	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
@@ -10,6 +11,34 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+		int choixUtilisateur;
+		if (!controlAcheterProduit.appartenirVillage(nomAcheteur)) {
+			System.out.println("Je suis désolée " + nomAcheteur + " mais il faut être un habitant de notre village pour commercer ici.\n");
+		} else {
+			do {
+				StringBuilder question = new StringBuilder();
+				question.append("1 - je veux acheter un produit.\n");
+				question.append("2 - je veux avoir une vue d'ensemble du marché.\n");
+				question.append("3 - quitter l'application.\n");
+				choixUtilisateur = Clavier.entrerEntier(question.toString());
+				switch (choixUtilisateur) {
+				case 1: {
+					//TODO
+					break;
+				}
+				case 2: {
+					boundaryAfficherMarche.afficherMarche(nomAcheteur);
+					break;
+				}
+				case 3: {
+					System.out.println("Au revoir client " + nomAcheteur + "\n");
+					break;
+				}
+				default:
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 ou 3 !\n");
+					break;
+				}
+			} while (choixUtilisateur!=3);
+		}
 	}
 }
